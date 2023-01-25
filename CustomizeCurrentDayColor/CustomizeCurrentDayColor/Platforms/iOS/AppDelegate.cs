0828f2f3a6b6a9260e7e070651ec61d62d224c5a
VishalOmprasad <vishal.omprasad@syncfusion.com>
2023-01-25 12:07:53 +0530

    New KB sample added

---
+++ CustomizeCurrentDayColor/CustomizeCurrentDayColor/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CustomizeCurrentDayColor;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
